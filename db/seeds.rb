b46b4a80346fe12320a581fa870e30f68b42ab8a
Pedro Gaspar <pedro.gaxpar@gmail.com>
2014-04-11 01:59:44 +0100

    Import top 250 from IMDB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+require 'get_subtitles'
+
+puts "Movie Count: #{Movie.count}"
+
+CSV.foreach('db/seed_data/imdb_250.csv') do |rnd, num_votes, rating, title|
+  title_without_year = title.split(' (').first
+  next if Movie.find_by(name: title_without_year)
+
+  begin
+    sub = FetchSubtitle.new.search(title, [OSDb::Search::IMDB])
+    sleep 2 if sub == false
+  end while sub == false
+
+  if sub
+    movie = Movie.find_by(imdb_id: sub.raw_data["IDMovieImdb"])
+    movie ||= Movie.create_from_sub(sub)
+    puts ">> Done with #{movie.name}"
+  end
+  puts
+end
+
+puts "Movie Count: #{Movie.count}"
