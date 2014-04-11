5a21cb65a757e716ecd49913fe63240b2f45c89c
Pedro Gaspar <pedro.gaxpar@gmail.com>
2014-04-11 12:13:44 +0100

    Adding more Seeds. OpenSubtitles is blocking us though 200 per day..

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,27 @@
 require 'csv'
 require 'get_subtitles'
 
-puts "Movie Count: #{Movie.count}"
+def import_250
+  CSV.foreach('db/seed_data/imdb_250.csv') do |rnd, num_votes, rating, title|
+    import_movie(title)
+  end
+end
 
-CSV.foreach('db/seed_data/imdb_250.csv') do |rnd, num_votes, rating, title|
+def filter_all
+  open('db/seed_data/imdb.csv').each do |line|
+    line = line.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+    rnd, num_votes, rating, title = line.match(/^\s+([^\s]+)\s+(\d+)\s+([\d.]+)\s+(.+)$/i).captures rescue next
+    
+    unless num_votes.to_i < 30000 || rating.to_f < 7.0
+      #puts title
+      import_movie(title)
+    end
+  end
+end
+
+def import_movie(title)
   title_without_year = title.split(' (').first
-  next if Movie.find_by(name: title_without_year)
+  return if Movie.find_by(name: title_without_year)
 
   begin
     sub = FetchSubtitle.new.search(title, [OSDb::Search::IMDB])
@@ -21,11 +37,24 @@ CSV.foreach('db/seed_data/imdb_250.csv') do |rnd, num_votes, rating, title|
   end while sub == false
 
   if sub
-    movie = Movie.find_by(imdb_id: sub.raw_data["IDMovieImdb"])
-    movie ||= Movie.create_from_sub(sub)
-    puts ">> Done with #{movie.name}"
+    begin
+      movie = Movie.find_by(imdb_id: sub.raw_data["IDMovieImdb"])
+      movie ||= Movie.create_from_sub(sub)
+      puts ">> Done with #{movie.name}"
+    rescue Zlib::GzipFile::Error => e
+      puts "Gzip Error - Probably because of Anti Leeching protection"
+    end
   end
   puts
 end
+
+#t: 310922
+#30000: 2735
+#30000 e > 7.0: 1359
+
+puts "Movie Count: #{Movie.count}"
+
+#import_250
+filter_all
 
 puts "Movie Count: #{Movie.count}"
