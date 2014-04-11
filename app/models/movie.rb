14804475aaab8b173475258967578808b7886037
Pedro Gaspar <pedro.gaxpar@gmail.com>
2014-04-11 14:50:26 +0100

    Using original_srt field...

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,12 +4,13 @@ class Movie < ActiveRecord::Base
 
   def self.create_from_sub(sub)
     self.create({
-      content:     sub.extract_text,
-      imdb_id:     sub.raw_data["IDMovieImdb"],
-      year:        sub.raw_data["MovieYear"],
-      filename:    sub.filename,
-      imdb_rating: sub.raw_data["MovieImdbRating"],
-      name:        sub.movie_name
+      content:      sub.extract_text,
+      original_srt: sub.body,
+      imdb_id:      sub.raw_data["IDMovieImdb"],
+      year:         sub.raw_data["MovieYear"],
+      filename:     sub.filename,
+      imdb_rating:  sub.raw_data["MovieImdbRating"],
+      name:         sub.movie_name
     })
   end
 end
